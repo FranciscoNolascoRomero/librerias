5c4703f0c5cc6c8e63f949d113ac27884f1ec047
FNR <francisnolascoromero@gmail.com>
2023-01-19 13:45:09 +0100

    12398

--- src/StringHelper.java
+++ src/StringHelper.java
@@ -51,8 +51,34 @@ public class StringHelper {
 
         return isSafePassword(8,12,"abcdefgha1Á_");
     }
-//    public static void main(String[] args) {
-//        System.out.println(issafepassword(8,12,"abcdefgha1Á_"));
-//        System.out.println(issafepasswordDefault("abcdefgha1Á_"));
-//    }
+    /**
+     * Methols
+     * @param number
+     * @return
+     * */
+    public static boolean checkPhoneNumber(String number){
+//        if (number.length()<9||number.length()>9) {
+//            return false;
+//        }
+//        //6,7,9
+//        if (!number.matches("[6,7,9][0-9]{8}")){
+//            return false;
+//        }
+
+
+        return number.matches("[679][0-9]{8}");
+    }
+    public static boolean isValideID(String ID) {
+        if (!ID.matches("[0-9]{8}[A-Za-z]")){
+            return false;
+        }
+
+        return ID.matches("");
+    }
+    public static void main(String[] args) {
+       System.out.println(isSafePassword(8,12,"abcdefgha1Á_"));
+        System.out.println(isSafePasswordDefault("abcdefgha1Á_"));
+        System.out.println(checkPhoneNumber("678988989"));
+    }
+
 }
