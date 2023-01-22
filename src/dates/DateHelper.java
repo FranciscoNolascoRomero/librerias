0d64678c0acb16b8e4f3ab8989f5223590133d99
francisco nolasco <francisnolascoromero@gmail.com>
2023-01-22 20:23:33 +0100

    123

--- src/dates/DateHelper.java
+++ src/dates/DateHelper.java
@@ -3,7 +3,7 @@ package dates;
 public class DateHelper {
     public static boolean checkDate(String date){
         String dias = "(0?[1-9]|1[0-9]|2[0-9]|3[01])";
-        String meses ="([1-9]|0[1-9]|1[0-2])";
+        String meses ="(3[01]|[12][0-9]|0?[1-9])";
         String annos="[12][0-9]{3}";
         String separadores ="[-/]";
         return date.matches(dias + separadores + meses + separadores + annos);
